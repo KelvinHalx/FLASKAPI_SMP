2437fe734512dfa17219a1a84e387f9c61d513b9
KelvinHalx <kelvinwainaina03@gmail.com>
2021-10-13 17:18:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,5 +19,19 @@ db = SQLAlchemy(app)
 #Initialize marshmallow
 ma = Marshmallow(app)
 
+# Product Class/Model
+class Product(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100), unique=True)
+  description = db.Column(db.String(200))
+  price = db.Column(db.Float)
+  qty = db.Column(db.Integer)
+
+  def __init__(self, name, description, price, qty):
+    self.name = name
+    self.description = description
+    self.price = price
+    self.qty = qty
+
 if __name__ == '__main__':
     app.run(debug=True)
